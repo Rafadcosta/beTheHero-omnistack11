81ac10378cebb859c3c166451ab2c9e460721010
Rafaela Duque <rafaeladuqu@gmail.com>
2020-03-28 19:34:34 -0300

    add routes, controllers, module cors

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,18 +1,21 @@
 const express = require('express');
 
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
+routes.post('/sessions',SessionController.create),
 
-    const body = request.body;
-    
-    console.log(body);
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-    return response.json({
-        evento: 'Semana omnistack 11.0',
-        aluna: 'Rafaela Duque',
-        github: 'Rafadcosta'
-    });
-});
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
