b574ae2c09821217a9cc1d26103434abde394f9c
Rafaela Duque <rafaeladuqu@gmail.com>
2020-03-28 23:05:17 -0300

    add assets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Header from './Header';
 
 function App() {
+  const [count, setCount]  = useState(0);
+
+  function increment(){
+    setCount ( count + 1 );
+  }
+
   return (
-    <h1>hello world</h1>
+    <div>
+      <Header>
+        Contador: {count}
+      </Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
+    
   );
 }
 
